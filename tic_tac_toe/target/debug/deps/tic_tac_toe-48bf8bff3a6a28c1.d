45619b99b166e7900b2ab9d935c9d7400d39352f
David Rees <astroponyrees@gmail.com>
2023-06-13 19:11:58 -0600

    Fleshed out structure of the voice command tic tac toe game project.

---
+++ tic_tac_toe/target/debug/deps/tic_tac_toe-48bf8bff3a6a28c1.d
@@ -0,0 +1,5 @@
+C:\Users\astro\Documents\Rust\tic_tac_toe\target\debug\deps\tic_tac_toe-48bf8bff3a6a28c1.rmeta: src\main.rs
+
+C:\Users\astro\Documents\Rust\tic_tac_toe\target\debug\deps\tic_tac_toe-48bf8bff3a6a28c1.d: src\main.rs
+
+src\main.rs:
---
+++ tic_tac_toe/target/debug/deps/tic_tac_toe-bc48134096038232.d
@@ -0,0 +1,5 @@
+C:\Users\astro\Documents\Rust\tic_tac_toe\target\debug\deps\tic_tac_toe-bc48134096038232.rmeta: src\main.rs
+
+C:\Users\astro\Documents\Rust\tic_tac_toe\target\debug\deps\tic_tac_toe-bc48134096038232.d: src\main.rs
+
+src\main.rs:
